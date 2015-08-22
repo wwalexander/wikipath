9dfda64754d6d21401e4b885d94bd926017805a5
Will Alexander <github.com@willalexander.me>
2015-08-22 16:44:52 -0400

    Refactor page grabbing to speed up search

--- wikipath.go
+++ wikipath.go
@@ -13,39 +13,9 @@ import (
 	"strings"
 )
 
-// APILink represents an link in an API response.
-type APILink struct {
-	Title string `json:"title"`
-}
-
-// APIPage represents a page in an API response.
-type APIPage struct {
-	Title   string     `json:"title"`
-	NS      int        `json:"ns"`
-	Missing *string    `json:"missing"`
-	Links   []*APILink `json:"links"`
-	Parent  *APIPage   `json:"-"`
-}
-
-// APIQuery represents a query response in an API response.
-type APIQuery struct {
-	Pages map[string]*APIPage `json:"pages"`
-}
-
-// APIContinue represents a pagination continue value in an API response.
-type APIContinue struct {
-	PLContinue string `json:"plcontinue"`
-	Continue   string `json:"continue"`
-}
-
-// APIResp represents an API response.
-type APIResp struct {
-	Query    APIQuery     `json:"query"`
-	Continue *APIContinue `json:"continue"`
-}
-
 var baseURL url.URL
 var baseQ url.Values
+var baseQLinks url.Values
 
 func init() {
 	baseURL = url.URL{
@@ -56,17 +26,51 @@ func init() {
 	baseQ = baseURL.Query()
 	baseQ.Set("action", "query")
 	baseQ.Set("format", "json")
-	baseQ.Set("prop", "links")
+	baseQLinks = baseQ
+	baseQLinks.Set("prop", "links")
 }
 
-// GetAPIResp gets an APIResp for the given title. If prev.Continue is not nil,
-// its values are used to continue pagination.
-func GetAPIResp(title string, prev *APIResp) (*APIResp, error) {
-	q := baseQ
-	if prev.Continue != nil {
-		q.Set("continue", prev.Continue.Continue)
-		q.Set("plcontinue", prev.Continue.PLContinue)
+// Page represents a Wikipedia page.
+type Page struct {
+	Title  string
+	Parent *Page
+	Links  []*Page
+}
+
+type apiPage struct {
+	Title   string  `json:"title"`
+	NS      int     `json:"ns"`
+	Missing *string `json:"missing"`
+	Links   []*Page `json:"links"`
+	Parent  *Page   `json:"-"`
+}
+
+type apiQuery struct {
+	Pages map[string]*apiPage `json:"pages"`
+}
+
+type apiContinue struct {
+	PLContinue string `json:"plcontinue"`
+	Continue   string `json:"continue"`
+}
+
+type apiResp struct {
+	Query    apiQuery     `json:"query"`
+	Continue *apiContinue `json:"continue"`
+}
+
+func (ar apiResp) apiPage() (*apiPage, error) {
+	for _, p := range ar.Query.Pages {
+		if p.Missing == nil && p.NS == 0 {
+			return p, nil
+		}
 	}
+	return nil, errors.New("page does not exist")
+}
+
+// GetPage gets a Page with the given title, without Links.
+func GetPage(title string) (*Page, error) {
+	q := baseQ
 	q.Set("titles", title)
 	url := baseURL
 	url.RawQuery = q.Encode()
@@ -75,46 +79,59 @@ func GetAPIResp(title string, prev *APIResp) (*APIResp, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	ar := &APIResp{}
-	if err = json.NewDecoder(resp.Body).Decode(ar); err != nil {
+	ar := apiResp{}
+	if err = json.NewDecoder(resp.Body).Decode(&ar); err != nil {
 		return nil, err
 	}
-	return ar, nil
+	ap, err := ar.apiPage()
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: ap.Title}, nil
 }
 
-// GetAPIPage gets an APIPage for the given title.
-func GetAPIPage(title string) (*APIPage, error) {
-	ap := &APIPage{
-		Title: title,
-		Links: []*APILink{},
+// PopulateLinks populates p.Links.
+func (p *Page) PopulateLinks() error {
+	q := baseQLinks
+	q.Set("titles", p.Title)
+	url := baseURL
+	url.RawQuery = q.Encode()
+	resp, err := http.Get(url.String())
+	if err != nil {
+		return err
 	}
-	prev := &APIResp{}
-	for {
-		ar, err := GetAPIResp(title, prev)
+	defer resp.Body.Close()
+	ar := apiResp{}
+	if err = json.NewDecoder(resp.Body).Decode(&ar); err != nil {
+		return err
+	}
+	for ar.Continue != nil {
+		ap, err := ar.apiPage()
 		if err != nil {
-			return nil, err
+			return err
 		}
-		var apPart *APIPage
-		for _, p := range ar.Query.Pages {
-			apPart = p
+		p.Links = append(p.Links, ap.Links...)
+		q.Set("continue", ar.Continue.Continue)
+		q.Set("plcontinue", ar.Continue.PLContinue)
+		url.RawQuery = q.Encode()
+		resp, err := http.Get(url.String())
+		if err != nil {
+			return err
 		}
-		if apPart == nil {
-			return nil, errors.New("page does not exist")
-		}
-		ap.Links = append(ap.Links, apPart.Links...)
-		prev = ar
-		if ar.Continue == nil {
-			break
+		defer resp.Body.Close()
+		ar = apiResp{}
+		if err = json.NewDecoder(resp.Body).Decode(&ar); err != nil {
+			return err
 		}
 	}
-	return ap, nil
+	return nil
 }
 
 // Path is the list of page titles between two pages (inclusive).
 type Path []string
 
 // NewPath creates a new Path by tracing t's Parents.
-func NewPath(t *APIPage) Path {
+func NewPath(t *Page) Path {
 	path := Path{}
 	for p := t; p != nil; p = p.Parent {
 		path = append(Path{p.Title}, path...)
@@ -127,59 +144,54 @@ func (p Path) String() string {
 	return strings.Join(p, " -> ")
 }
 
-// Valid returns whether ap exists and is non-namespaced
-func (ap APIPage) Valid() bool {
-	return ap.Missing == nil && ap.NS == 0
-}
-
-// Walk returns the shortest path between the pages with titles s and t. If no
-// path exists, ok is false.
-func Walk(s string, t string) (p Path, ok bool) {
+// Walk returns the shortest path between the pages with titles s and t.
+func Walk(s string, t string) (p Path, err error) {
 	visited := make(map[string]bool)
-	sPage, err := GetAPIPage(s)
-	if err != nil || !sPage.Valid() {
-		return nil, false
+	sp, err := GetPage(s)
+	if err != nil {
+		return nil, err
 	}
 	if s == t {
-		return NewPath(&APIPage{Title: s}), true
+		return NewPath(sp), nil
 	}
-	queue := []*APIPage{sPage}
+	if err = sp.PopulateLinks(); err != nil {
+		return nil, err
+	}
+	queue := []*Page{sp}
 	for len(queue) > 0 {
 		top := queue[0]
 		queue = queue[1:]
-		if !top.Valid() {
-			continue
-		}
 		log.Println(top.Title)
-		pages := make(chan *APIPage, len(top.Links))
+		pages := make(chan *Page, len(top.Links))
 		for _, l := range top.Links {
-			go func(title string) {
-				if _, ok := visited[title]; ok {
+			go func(p *Page) {
+				if _, ok := visited[p.Title]; ok {
 					pages <- nil
 					return
 				}
-				ap, err := GetAPIPage(title)
-				if err != nil {
-					pages <- nil
-					return
+				p.Parent = top
+				if p.Title != t {
+					if err = p.PopulateLinks(); err != nil {
+						pages <- nil
+						return
+					}
 				}
-				ap.Parent = top
-				pages <- ap
-			}(l.Title)
+				pages <- p
+			}(l)
 		}
 		for range top.Links {
-			ap := <-pages
-			if ap == nil {
+			p := <-pages
+			if p == nil {
 				continue
 			}
-			if ap.Title == t {
-				return NewPath(ap), true
+			if p.Title == t {
+				return NewPath(p), nil
 			}
-			visited[ap.Title] = true
-			queue = append(queue, ap)
+			visited[p.Title] = true
+			queue = append(queue, p)
 		}
 	}
-	return nil, false
+	return nil, errors.New("no path exists")
 }
 
 func main() {
@@ -191,9 +203,9 @@ func main() {
 	}
 	s := args[0]
 	t := args[1]
-	path, ok := Walk(s, t)
-	if !ok {
-		log.Fatalf("no path exists between %s and %s", s, t)
+	path, err := Walk(s, t)
+	if err != nil {
+		log.Fatal(err)
 	}
 	fmt.Println(path)
 }
